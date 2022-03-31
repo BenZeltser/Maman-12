8ccdd4fdb4328d8e940887d2a5e15ded262f64fd
Ben Zeltser <92685838+BenZeltser@users.noreply.github.com>
2022-03-30 20:10:28 -0400

    Add files via upload

---
+++ lists_ab.c
@@ -0,0 +1,36 @@
+/* Project Author: Ben Zeltser
+   ID: 313327579
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/*The program will continiuesly receive user input and will display it.
+  While the program will also sum the amount of Alpha-numeric numbers
+  and the total amount of tabs that we finished reading.
+  Following that, the program will print the two sums.*/
+
+  /*Input pointer*/
+
+  char* scan(char*);
+
+int main() {
+
+    char* input;
+    scan(input);
+    free(input);
+    return 0;
+    }
+char* scan(char *user_input) {
+    int flag, index;
+
+    /*Init to userInput is /0*/
+    user_input[0] = '\0'
+
+    /*iterate through the file using the index. finish when reached EOF (End Of File)*/
+    while(flag!=EOF) {
+        /*STOPPED HERE
+        finish comment...*/
+        if (flag.getchar())}
+    }
